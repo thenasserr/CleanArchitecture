f3aac62cc993520cffd35e9071bad07cf9adbe22
Ibrahim Nasser <thenasserr@gmail.com>
2024-02-29 15:36:40 +0200

    Initial Commit

---
+++ CleanArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanArchitecture
+//
+//  Created by Ibrahim Nasser Ibrahim on 29/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
